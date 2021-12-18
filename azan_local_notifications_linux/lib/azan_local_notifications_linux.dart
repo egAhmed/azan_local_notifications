6d4d0b80a49314727ae15acd253bfcf6c30e8f79
egAhmed <a7meda00@gmail.com>
2021-12-18 10:01:28 +0200

    add file

--- azan_local_notifications_linux/lib/azan_local_notifications_linux.dart
+++ azan_local_notifications_linux/lib/azan_local_notifications_linux.dart
@@ -1,7 +1,18 @@
 library azan_local_notifications_linux;
+/// The Linux implementation of `azan_local_notifications`.
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// azan_local_notifications_linux depends on dbus and posix
+// which uses FFI internally; export a stub for platforms that don't
+// support FFI (e.g., web) to avoid having transitive dependencies
+// break web compilation.
+export 'src/flutter_local_notifications_stub.dart'
+    if (dart.library.ffi) 'src/flutter_local_notifications.dart';
+export 'src/model/capabilities.dart';
+export 'src/model/categories.dart';
+export 'src/model/enums.dart';
+export 'src/model/icon.dart';
+export 'src/model/initialization_settings.dart';
+export 'src/model/location.dart';
+export 'src/model/notification_details.dart';
+export 'src/model/sound.dart';
+export 'src/model/timeout.dart';
